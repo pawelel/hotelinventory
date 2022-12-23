7cc80ec11128576873e88ae2e6a0dbfbb6c0628e
Paweł Przytuła <pawelelprzytula@gmail.com>
2022-12-23 01:19:39 +0100

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  // template: ` <h1>Hello 🌻</h1> `,
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
