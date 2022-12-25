4eec4085d006097d7f95ce56e45a1b8d958cc2ce
Paweł Przytuła <pawelelprzytula@gmail.com>
2022-12-25 20:40:56 +0000

    Dec 25, 2022, 9:40 PM

--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -68,19 +68,5 @@ export class RoomsComponent implements OnInit {
   selectRoom(room: IRoom) {
     this.selectedRoom = room;
   }
-  addRoom() {
-    const newRoom: IRoom = { 
-    id : this.roomList.length + 1,
-    status : 'Available',
-    checkInDate : new Date(),
-    checkOutDate : new Date(),
-    anemities : ['TV', 'AC', 'Wifi'],
-    image : 'https://i.picsum.photos/id/607/200/300.jpg',
-    description : 'A room without doors',
-    number : 300 + this.roomList.length,
-    type : 'Deluxe',
-    price : 100+this.roomList.length,
-    };
-    this.roomList.push(newRoom);
-  }
+  
 }
